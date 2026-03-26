9f94a1953abaea54184b05710d96de3d4922ccbe
KB-Designs <kimanibrian030@gmail.com>
2026-03-26 20:25:28 +0300

    added source codes

--- src/App.js
+++ src/App.js
@@ -34,16 +34,16 @@ export default function App() {
       title: "Crave & Glaze | Bakery Platform ",
       description: "Full-stack bakery e-commerce platform featuring dynamic product variants, M-Pesa payment integration, admin management, Dockerized deployment, and asynchronous email notifications.",
       tech: ["React", "Node.js", "MongoDB", "Stripe"],
-      liveUrl: "https://example.com",
-      codeUrl: "https://github.com/yourusername/project1",
+      liveUrl: "https://crave-and-glaze-final.onrender.com",
+      codeUrl: "https://github.com/kimani-brian/shikagari",
       image: "https://images.unsplash.com/photo-1557821552-17105176677c?w=800&h=500&fit=crop"
     },
     {
       title: "InstaMart - E-Commerce Platform",
       description: "Full-stack e-commerce platform with M-Pesa payment integration, session-based cart management, secure authentication, and Dockerized deployment.",
       tech: ["Go", "Postgres", "Tailwind", "Javascript"],
-      liveUrl: "https://example.com",
-      codeUrl: "https://github.com/yourusername/project2",
+      liveUrl: "https://instamart-3vmc.onrender.com",
+      codeUrl: "https://github.com/kimani-brian/myshop-continua",
       image: "https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?w=800&h=500&fit=crop"
     },
     
